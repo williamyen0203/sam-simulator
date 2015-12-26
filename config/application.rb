5c9533b861d0332782e19e75e4cd79d84595f35d
William Yen <williamyen0203@gmail.com>
2015-12-25 22:50:07 -0500

    Asset precompile fix 2

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module SamSimulator
       'hella-tite.css'
     ]
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.compile = true
+    config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   end
 end
